9c6b34f1e1070b9e3d00cfcabae4da3e6b9c3b55
jupiterangulo <42626972+jupiterangulo@users.noreply.github.com>
2018-08-22 19:59:21 -0500

    additional changes to formatting

--- draftOrderPublic.py
+++ draftOrderPublic.py
@@ -4,15 +4,15 @@ import time
 
 # create function for Draft Order 
 def assignDraftOrder():
+  
   # enter number of players in your league
-  size = 10 #default to 10 
+  size = 10 #default to 10
+  
   # insert players names here 
   players = ['Bob', 'Ray', 'Fred', 'Leroy', 'Otis',
              'Chet', 'Chad', 'Miles', 'Guy', 'Clay']
   
-  # list out players as they appear in the fantasy football league 
   print ('The order of players as they joined is:')
-  
   for i in range(0,size):
     j = i + 1
     print ("{}. {}".format(j, players[i]))
@@ -28,7 +28,7 @@ def assignDraftOrder():
 
   # one more dramatic pause because why not
   time.sleep(3)
-  for i in range(0,10):
+  for i in range(0,size):
     j = i + 1
     print ("{}. {}".format(j, draftOrder[i]))
     time.sleep(3)
